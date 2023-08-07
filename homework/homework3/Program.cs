4f6cf29313c057bcf6a1ed0172a580bf156f8cb2
Maria <ec39sp@yandex.ru>
2023-08-07 21:16:57 +0200

    home3

---
+++ homework/homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num%2==0)
+{
+    Console.WriteLine($"{num} - четное число");
+}
+else
+{
+    Console.WriteLine($"{num} - нечетное число");
+}
