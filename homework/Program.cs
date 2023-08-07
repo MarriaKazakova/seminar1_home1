5b600580a94cba5a83c064cfde229f9fbf55c1c9
Maria <ec39sp@yandex.ru>
2023-08-07 20:36:50 +0200

    home_1

---
+++ homework/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine($"Максимальное число равно {num1}, минимальное число равно {num2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число равно {num2}, минимальное число равно {num1}");
+}
