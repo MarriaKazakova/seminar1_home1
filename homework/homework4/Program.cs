84da05c224a9fdf396a98d2911d8167e348d1506
Maria <ec39sp@yandex.ru>
2023-08-07 22:29:31 +0200

    home4

---
+++ homework/homework4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+        List<int> numbers = new List<int>();
+        for (int i = 2; i <= num; i=i+2)
+        {
+                numbers.Add(i);
+        }
+        Console.WriteLine("Вот ваш список:");
+        foreach (int number in numbers)
+        {
+            Console.Write(" " + number);
+        }
